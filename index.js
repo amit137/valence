e487420262734b84c27d73435eebfc3e5a22610f
Amit Singh <amitsinghjin13@gmail.com>
2023-03-04 17:06:21 +0530

    apis

---
+++ index.js
@@ -0,0 +1,37 @@
+const express=require('express')
+const app=express();
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+const helmet=require('helmet')
+const morgan=require('morgan')
+const cors=require('cors')
+
+const userRoute=require('./routes/users')
+const authRoute=require('./routes/auth')
+const postsRoute=require('./routes/posts')
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}
+).then(()=>console.log('Connected to db'))
+.catch((err)=>console.log(err))
+
+//middlewares
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
+app.use(cors())
+
+
+app.use('/api/users',userRoute)
+app.use('/api/auth',authRoute)
+app.use('/api/posts',postsRoute)
+
+
+app.listen(8080,()=>{
+    console.log('Server is listening')
+})
+
---
+++ models/User.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 4,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    desc: {
+      type: String,
+      max: 100,
+    },
+    city: {
+      type: String,
+      max: 50,
+    },
+    from: {
+      type: String,
+      max: 50,
+    },
+    relationship: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
